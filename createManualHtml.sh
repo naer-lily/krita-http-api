7a2e42eea14219ce093e67d5b29004153d327e3f
NAER <V-YOP@pm.me>
2026-05-11 01:06:26 +0800

    chore: remove unused files (benchmark.js, kra-curl, temp/, createManualHtml.sh, test_tiled.py); regenerate Manual.html from README; gitignore botpy.log

--- createManualHtml.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-pandoc -o krita_http_api/Manual.html README.md
--- krita_http_api/temp/settingTest.py
+++
@@ -1,4 +0,0 @@
-from krita import *
-
-#  theme ["Theme"]
-print(Krita.instance().readSetting('theme', 'Theme', 'EMPTY'))
